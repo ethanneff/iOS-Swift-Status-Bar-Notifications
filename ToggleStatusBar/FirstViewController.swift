ba7768d2f88d5be3d97420ec81d64aed6506532e
Ethan Neff <ethan.neff@eneff.com>
2016-01-06 11:58:08 -0800

    Initial Commit

---
+++ ToggleStatusBar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ToggleStatusBar
+//
+//  Created by Ethan Neff on 1/6/16.
+//  Copyright © 2016 Ethan Neff. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ ToggleStatusBar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToggleStatusBar
+//
+//  Created by Ethan Neff on 1/6/16.
+//  Copyright © 2016 Ethan Neff. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
